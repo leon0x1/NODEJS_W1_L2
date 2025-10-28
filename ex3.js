1113006fbcde902d240ecf45a4e2a0ad0bd1e632
Leon <leonmavrin181@gmail.com>
2025-10-28 15:30:34 +0200

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -5,8 +5,9 @@
 let arr = [0, 5, 0, 2, 3, 0, 3, 11, 0];
 let count = 0;
 
-for (let i = 0; i < arr.length; i++) {
-  arr[i] === 0 ? count++ : (count = count);
-}
+arr.forEach(element => {
+  element === 0 ? count++ : (count = count);
+});
 
 console.log(count);
+
