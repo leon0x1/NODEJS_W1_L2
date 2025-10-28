aefba273aa57ca316c8c92ceb6c836f02f60e6db
Leon <leonmavrin181@gmail.com>
2025-10-28 15:36:38 +0200

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,19 +1,18 @@
-'use strict';
+/*
+שמות מגישים: 
+יאיל סאלם
+ליאון מברין
+*/
+const num = 7;
 
-// מגישים:
-// ליאון מברין
-// יאיל סאלם
+// Saving the boolean result of the division and casting it to a number
+let divisionResult =
+  Number(num % 2 === 0 || num % 3 === 0 || num % 5 === 0) +
+  Number(
+    (num % 2 === 0 && num % 3 === 0) ||
+      (num % 3 === 0 && num % 5 === 0) ||
+      (num % 2 === 0 && num % 5 === 0)
+  ) +
+  Number(num % 2 === 0 && num % 3 === 0 && num % 5 === 0);
 
-const n = 30;
-
-let flag1 = n % 2 === 0 || n % 3 === 0 || n % 5 === 0;
-
-let flag2 =
-  (n % 2 === 0 && n % 3 === 0) ||
-  (n % 3 === 0 && n % 5 === 0) ||
-  (n % 2 === 0 && n % 5 === 0);
-
-let flag3 = n % 2 === 0 && n % 3 === 0 && n % 5 === 0;
-
-console.log(Number(flag1) + Number(flag2) + Number(flag3));
-
+console.log(divisionResult);
