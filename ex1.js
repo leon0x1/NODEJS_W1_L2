bd22e5bc5f609ab6304b5d04765b773786653c08
Leon <leonmavrin181@gmail.com>
2025-10-28 15:32:36 +0200

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// מגיש: ליאון מברין
+// מגישים:
+// ליאון מברין
+// יאיל סאלם
 
 const n = 30;
 
@@ -14,3 +16,4 @@ let flag2 =
 let flag3 = n % 2 === 0 && n % 3 === 0 && n % 5 === 0;
 
 console.log(Number(flag1) + Number(flag2) + Number(flag3));
+
